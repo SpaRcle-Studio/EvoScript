2511d6d11ed8869af09d63425c1d576cca623cdf
Monika0000 <rotaru5craft@gmail.com>
2021-07-31 22:06:19 +0300

    1. Add fbx loader 2. Complete scene GUI for OpenGL/Vulkan

--- Core/inc/EvoScript/Win32State.hpp
+++ Core/inc/EvoScript/Win32State.hpp
@@ -11,6 +11,30 @@
 #include <EvoScript/IState.h>
 
 namespace EvoScript {
+    //Returns the last Win32 error, in string format. Returns an empty string if there is no error.
+    static std::string GetLastErrorAsString() {
+        //Get the error message ID, if any.
+        DWORD errorMessageID = ::GetLastError();
+        if(errorMessageID == 0) {
+            return std::string(); //No error message has been recorded
+        }
+
+        LPSTR messageBuffer = nullptr;
+
+        //Ask Win32 to give us the string version of that message ID.
+        //The parameters we pass in, tell Win32 to create the buffer that holds the message for us (because we don't yet know how long the message string will be).
+        size_t size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+                                     NULL, errorMessageID, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&messageBuffer, 0, NULL);
+
+        //Copy the error message into a std::string.
+        std::string message(messageBuffer, size);
+
+        //Free the Win32's string's buffer.
+        LocalFree(messageBuffer);
+
+        return message;
+    }
+
     class Win32State : public IState {
     public:
         Win32State(const Win32State&) = delete;
@@ -52,9 +76,17 @@ namespace EvoScript {
                 return false;
             }
 
+            if (!Tools::FileExists(m_path)) {
+                ES_ERROR("Win32State::Load() : file is not exists! \n\tPath: " + m_path)
+                return false;
+            }
+
             this->m_hDLL = LoadLibrary(m_path.c_str());
             if (!m_hDLL) {
-                ES_ERROR("Win32State::Load() : failed to load dll!")
+                ES_ERROR("Win32State::Load() : failed to load dll!"
+                         "\n\tPath: " + m_path +
+                         "\n\tError code: " + std::to_string(GetLastError()) +
+                         "\n\tDescription: " + GetLastErrorAsString());
                 return false;
             }
 
