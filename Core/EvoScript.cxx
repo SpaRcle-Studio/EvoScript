c98c50a9123584cff85e85061ba9668e1412f930
Monika0000 <rotaru5craft@gmail.com>
2023-10-22 02:39:52 +0200

    (Android) Fixed compilation for android

---
+++ Core/EvoScript.cxx
@@ -0,0 +1,12 @@
+#include "src/EvoScript/Compilation/Casting.cpp"
+#include "src/EvoScript/Compilation/Compiler.cpp"
+#include "src/EvoScript/Compilation/AddressTableGen.cpp"
+#include "src/EvoScript/Compilation/Method.cpp"
+#include "src/EvoScript/Compilation/CMakeCodeGen.cpp"
+
+#include "src/EvoScript/Script.cpp"
+#include "src/EvoScript/IState.cpp"
+
+#ifdef WIN32
+    #include "inc/EvoScript/Win32State.hpp"
+#endif
